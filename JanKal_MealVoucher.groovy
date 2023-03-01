6a87ec49ee5ebaf36cb629abb828d0e613eb4530
JansenSmith <jansen@bstarengineering.com>
2023-03-01 02:07:05 -0500

    moved from inkscape text to bowler text

--- JanKal_MealVoucher.groovy
+++ JanKal_MealVoucher.groovy
@@ -1,10 +1,8 @@
-import eu.mihosoft.vrl.v3d.svg.*;
-
 import com.neuronrobotics.bowlerstudio.scripting.ScriptingEngine
-
+import eu.mihosoft.vrl.v3d.*
+import eu.mihosoft.vrl.v3d.svg.*
 import eu.mihosoft.vrl.v3d.CSG
-import eu.mihosoft.vrl.v3d.Extrude;
-import eu.mihosoft.vrl.v3d.Polygon
+import javafx.scene.text.Font;
 
 File f = ScriptingEngine
 	.fileFromGit(
@@ -19,15 +17,40 @@ println "Layers= "+s.getLayers()
 HashMap<String,List<Polygon>> polygonsByLayer = s.toPolygons()
 // extrude all layers to a map to 10mm thick
 HashMap<String,ArrayList<CSG>> csgByLayers = s.extrudeLayers(10)
-// extrude just one layer to 10mm
-// The string "1-holes" represents the layer name in Inkscape
-def holeParts = s.extrudeLayerToCSG(10,"1-holes")
-// seperate holes and outsides using layers to differentiate
-// The string "2-outsides" represents the layer name in Inkscape
-def outsideParts = s.extrudeLayerToCSG(10,"2-outsides")
-					.difference(holeParts)
-// layers can be extruded at different depths
-// The string "3-boarder" represents the layer name in Inkscape					
-def boarderParts = s.extrudeLayerToCSG(5,"3-boarder")
+// The string represents the layer name in Inkscape
+def border = s.extrudeLayerToCSG(7,"Border")
+CSG voucher = border
+def ticket = s.extrudeLayerToCSG(10,"Ticket")
+voucher = voucher.union(ticket)
+def lines = s.extrudeLayerToCSG(3,"Lines")
+lines = lines.movez(7)
+voucher = voucher.difference(lines)
+def relief = s.extrudeLayerToCSG(3,"Relief")
+relief = relief.movez(7)//.hull(relief.toolOffset(2))
+voucher = voucher.difference(relief)
+def innards = s.extrudeLayerToCSG(3,"Innards")
+innards = innards.movez(7)
+voucher = voucher.union(innards)
 
-return [CSG.unionAll([boarderParts,outsideParts]),s.extrudeLayerToCSG(2,"4-star")]
+Font ribbonFont = new Font("Arial",  5)
+//Font ribbonFont = new Font("Constantia",  5)
+Font playfulFont = new Font("Arial",  7)
+//Font playfulFont = new Font("MV Boli",  30)
+
+CSG ribbonText = CSG.unionAll(TextExtrude.text((double)3.0,"ONE MEAL",ribbonFont))
+	.rotx(180).toZMin().centerx().centery()
+	.movex(relief.getCenterX())
+	.movey(relief.getCenterY()-3.5)
+	.movez(7)
+voucher = voucher.union(ribbonText)
+
+CSG enjoyText = CSG.unionAll(TextExtrude.text((double)3.0,"ENJOY A MEAL",playfulFont))
+	.rotx(180).toZMin().centerx().toYMax()
+	.movex(relief.getCenterX())
+	.movey(lines.getMaxY())
+	.movez(7)
+println(enjoyText.getCenterX())
+voucher = voucher.difference(enjoyText)
+
+return voucher.setColor(javafx.scene.paint.Color.MAGENTA)
+//return ribbonText
